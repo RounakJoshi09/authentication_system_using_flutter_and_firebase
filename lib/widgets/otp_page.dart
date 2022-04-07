6821219fea57084ac389fcd717428afacfbc1d0e
Rounak <rounakjsh783@gmail.com>
2022-04-07 14:12:56 +0530

    fonts added and Button Designed

--- lib/widgets/otp_page.dart
+++ lib/widgets/otp_page.dart
@@ -9,19 +9,43 @@ class OTPPage extends StatelessWidget {
     return Container(
       width: double.infinity,
       margin: EdgeInsets.symmetric(vertical: 22, horizontal: 10),
-      child: OTPTextField(
-        length: 6,
-        width: MediaQuery.of(context).size.width,
-        fieldWidth: 40,
-        fieldStyle: FieldStyle.box,
-        style: TextStyle(fontSize: 20),
-        otpFieldStyle: OtpFieldStyle(
-          backgroundColor: Color.fromARGB(66, 14, 153, 247),
-        ),
-        textFieldAlignment: MainAxisAlignment.spaceAround,
-        onCompleted: (pin) {
-          print("Completed: " + pin);
-        },
+      child: Column(
+        children: [
+          OTPTextField(
+            length: 6,
+            width: MediaQuery.of(context).size.width,
+            fieldWidth: 40,
+            fieldStyle: FieldStyle.box,
+            style: TextStyle(fontSize: 20),
+            otpFieldStyle: OtpFieldStyle(
+              backgroundColor: Color.fromARGB(66, 14, 153, 247),
+            ),
+            textFieldAlignment: MainAxisAlignment.spaceAround,
+            onCompleted: (pin) {
+              print("Completed: " + pin);
+            },
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+            child: MaterialButton(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                child: Text(
+                  'VERIFY',
+                  textScaleFactor: 1.5,
+                  style: TextStyle(
+                    fontFamily: 'RobotoMono',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              color: Color.fromARGB(255, 6, 7, 122),
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/phone_number.dart
+++ lib/widgets/phone_number.dart
@@ -1,27 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneNumber extends StatelessWidget {
   final phoneNumberController;
+  FirebaseAuth _auth = FirebaseAuth.instance;
   PhoneNumber({this.phoneNumberController});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 25, horizontal: 5),
-      child: IntlPhoneField(
-        controller: phoneNumberController,
-        decoration: InputDecoration(
-          labelText: 'Phone Number',
-          border: OutlineInputBorder(
-            borderSide: BorderSide(),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+          child: IntlPhoneField(
+            controller: phoneNumberController,
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(),
+              ),
+            ),
+            initialCountryCode: 'IN',
+            onChanged: (phone) {
+              print(phone.completeNumber);
+            },
+            showDropdownIcon: false,
           ),
         ),
-        initialCountryCode: 'IN',
-        onChanged: (phone) {
-          print(phone.completeNumber);
-        },
-        showDropdownIcon: false,
-      ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+          child: MaterialButton(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              child: Text(
+                'CONTINUE',
+                textScaleFactor: 1.5,
+                style: TextStyle(fontFamily: 'RobotoMono', color: Colors.white),
+              ),
+            ),
+            color: Color.fromARGB(255, 6, 7, 122),
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+        ),
+      ],
     );
   }
 }
